dcc88ff5d8dce37f103e4f6752eb1706e0245c2a
Sahilramjali <sahil.ramjali13@gmail.com>
2023-06-12 09:55:06 +0545

    sth went wrong

--- src/componets/clients.jsx
+++ src/componets/clients.jsx
@@ -2,16 +2,9 @@ import { clients } from "../constants/index";
 import styles from "../style";
 const Clinets = () => {
   return (
-    <section className={`${styles.flexCenter} my-4`}>
-      <div className={`${styles.flexCenter} felx-wrap w-full`}>
-        {clients.map((client) => (
-            <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
-                <img  src={client.logo} alt={client.id} className="sm:w-[192px] w-[100px] h-[60px] object-contain"/>
-            </div>
-          
-        ))}
-      </div>
-    </section>
+    <div>
+        clients
+    </div>
   );
 };
 export default Clinets;
