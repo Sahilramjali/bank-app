4f8524c426b647e76570c9115bd55390e9aa4220
Sahilramjali <sahil.ramjali13@gmail.com>
2023-06-12 09:57:00 +0545

    clients

--- src/componets/clients.jsx
+++ src/componets/clients.jsx
@@ -2,9 +2,22 @@ import { clients } from "../constants/index";
 import styles from "../style";
 const Clinets = () => {
   return (
-    <div>
-        clients
-    </div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full `}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5 `}
+          >
+            <img
+              src={client.logo}
+              alt={client.id}
+              className="sm:w-[192px] w-[100px] h-[60px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 export default Clinets;
